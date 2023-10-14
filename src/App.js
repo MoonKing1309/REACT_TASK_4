39fd16fc62d1d1eaf2bd07c70fdb32e4f6dcfd7b
MoonKing1309 <conorken1@gmail.com>
2023-10-14 21:25:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { forEach } from 'mathjs';
+import {useState,useEffect,Component} from 'react';
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data,setData] = useState([]);
+
+  useEffect(()=>
+  {
+    Axios.get("https://dummyjson.com/users")
+    .then((res)=>
+    { 
+      console.log(res.data.users)
+        setData(res.data.users);
+    })
+    .catch((err)=>alert(err))
+  },[])
+
+  function fetchDetails()
+  {
+    return data.map((x,i)=>{
+      return(
+          <tbody key={i}>
+            <tr>
+              <td>{x.id}</td>
+              <td><img src={x.image} alt="pfp" width={'50px'}></img></td>
+              <td>{x.firstName}</td>
+              <td>{x.lastName}</td>
+              <td>{x.gender}</td>
+              <td>{x.email}</td>
+              <td>{x.username}</td>
+              <td>{x.domain}</td>
+              <td>{x.ip}</td>
+              <td>{x.university}</td>
+            </tr>
+          </tbody> 
+      )
+    })
+  }
+  
+  return(
+        <div>
+          <h1>Dummy Data</h1>
+          <table border={'2px'}>
+          <tbody>
+            <tr style={{border:"2px solid white"}}>
+              <th>Sno</th>
+              <th>Profile Pic</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Gender</th>
+              <th>E-mail</th>
+              <th>Username</th>
+              <th>Domain</th>
+              <th>IP</th>
+              <th>University</th>
+            </tr>
+          </tbody>
+          {
+            fetchDetails()
+          }
+        </table>
+        </div>
+        
+  )
 }
 
-export default App;
+export default App
